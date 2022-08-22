1a214cede918e7609935b985a1391a7727d5577e
dev_kelo <clamp1981@gmail.com>
2022-08-22 14:24:28 +0900

    use the code behind DoubleAnimation

--- WPF-Animations/AnimationDemo/AnimationDemo/MainWindow.xaml.cs
+++ WPF-Animations/AnimationDemo/AnimationDemo/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,25 @@ namespace AnimationDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GridForCode_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0.0d;
+            da.To = 1.0d;
+            da.Duration = new Duration(new TimeSpan(0, 0, 10));
+            LoadedEventLabelUseInCode.BeginAnimation(Grid.OpacityProperty, da);
+
+        }
+
+        private void GridForCode_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0.0d;
+            da.To = 1.0d;
+            da.Duration = new Duration(new TimeSpan(0, 0, 10));
+            MouseEventLabelUseInCode.BeginAnimation(Grid.OpacityProperty, da);
         }
     }
 }
