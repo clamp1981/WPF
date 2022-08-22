448e0956f495ebfe95981f2cfa7150beec7b125d
dev_kelo <clamp1981@gmail.com>
2022-08-22 11:45:50 +0900

    use event and usercontrol

---
+++ WPF-Navigation/NavigationDemo/DelegateAndEvent.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NavigationDemo
+{
+    /// <summary>
+    /// Interaction logic for DelegateAndEvent.xaml
+    /// </summary>
+    public partial class DelegateAndEvent : Window
+    {
+        public DelegateAndEvent()
+        {
+            InitializeComponent();
+            ValueController.MinThresHoldReached += ValueController_MinThresHoldReached;
+            ValueController.MaxThresHoldReached += ValueController_MaxThresHoldReached;
+        }
+
+        private void ValueController_MaxThresHoldReached(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Maximum value reached!!");
+        }
+
+        private void ValueController_MinThresHoldReached(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Minimum value reached!!");
+        }
+    }
+}
---
+++ WPF-Navigation/NavigationDemo/ValueUC.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NavigationDemo
+{
+    /// <summary>
+    /// Interaction logic for ValueUC.xaml
+    /// </summary>
+    public partial class ValueUC : UserControl
+    {
+
+        public event EventHandler<RoutedEventArgs> MinThresHoldReached;
+        public virtual void OnMinThresHoldReached(RoutedEventArgs e)
+        {
+            MinThresHoldReached?.Invoke(this, e);
+        }
+
+        public event EventHandler<RoutedEventArgs> MaxThresHoldReached;
+        public virtual void OnMaxThresHoldReached(RoutedEventArgs e)
+        {
+            MaxThresHoldReached?.Invoke(this, e);
+        }
+        public ValueUC()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxValue.Text = (Convert.ToInt32(textBoxValue.Text) + 10).ToString();
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxValue.Text = (Convert.ToInt32(textBoxValue.Text) - 10).ToString();
+        }
+
+        private void textBoxValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int value = 0;
+            if( Int32.TryParse((sender as TextBox).Text, out value) )
+            {
+                if( value < 0 )
+                {
+                    (sender as TextBox).Text = "0";
+                    OnMinThresHoldReached(e);
+                }
+                else if( value >= 100 )
+                {
+                    (sender as TextBox).Text = "100";
+                    OnMaxThresHoldReached(e);
+                }
+
+            }
+           
+                
+        }
+    }
+}
