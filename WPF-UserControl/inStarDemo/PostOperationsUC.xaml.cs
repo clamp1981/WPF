03a0e704668bcd7973fd9beb37b5368a3a468fdf
dev_kelo <clamp1981@gmail.com>
2022-08-23 11:09:58 +0900

    create usercontrol project

---
+++ WPF-UserControl/inStarDemo/PostOperationsUC.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace inStaDemo
+{
+    /// <summary>
+    /// Interaction logic for PostOperationsUC.xaml
+    /// </summary>
+    public partial class PostOperationsUC : UserControl
+    {
+        public bool PostLiked { get; set; }
+        public PostOperationsUC()
+        {
+            InitializeComponent();
+            PostLiked = false;
+        }
+
+        private void imageLike_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string uri = Environment.CurrentDirectory + @"\..\..\icons\";
+            uri += PostLiked ? "like.png" : "liked.png";
+            imageLike.Source = new BitmapImage(new Uri(uri, UriKind.RelativeOrAbsolute));
+            PostLiked = !PostLiked;
+        }
+    }
+}
