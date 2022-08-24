b8c1f26141c7cd023012b087d749be957cc4207c
dev_kelo <clamp1981@gmail.com>
2022-08-24 15:51:19 +0900

    create project

---
+++ WPF-DependencyProperties/DependencyProperties/MyDpSampleUC.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyProperties
+{
+    /// <summary>
+    /// Interaction logic for MyDpSampleUC.xaml
+    /// </summary>
+    public partial class MyDpSampleUC : UserControl
+    {
+        
+        public int MyProperty
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(MyDpSampleUC), new PropertyMetadata(0));
+
+
+        public MyDpSampleUC()
+        {
+            
+            InitializeComponent();
+            this.DataContext = this;
+
+        }
+    }
+}
