5eaba04890830c9d09ed8c556121062fb72e92e5
dev_kelo <clamp1981@gmail.com>
2022-08-29 10:41:09 +0900

    fix for pull error

--- WPF-UsingMahApps/MSAppStoreClone/MainWindow.xaml.cs
+++ WPF-UsingMahApps/MSAppStoreClone/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MSAppStoreClone.Pages;
+using MSAppStoreClone.UserControls;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,34 @@ namespace MSAppStoreClone
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public MenuUC HomeMenu { get; set; }
+        public MenuUC AppMenu { get; set; }
+        public MenuUC GameMenu { get; set; }
+        public MenuUC LibraryMenu { get; set; }
+        public MenuUC HelpMenu { get; set; }
+
+
+        public AppsPage AppPage { get; set; }
+        public HomePage HomePage { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            //Uri uri = new Uri("pack://application:,,,/Images/Menu/home.png");
+            HomeMenu = new MenuUC(Environment.CurrentDirectory + @"\..\..\Images\Menu\home.png", Common.MenuTypes.Home);
+            AppMenu = new MenuUC(Environment.CurrentDirectory + @"\..\..\Images\Menu\Apps.png", Common.MenuTypes.Apps);
+            GameMenu = new MenuUC(Environment.CurrentDirectory + @"\..\..\Images\Menu\game.png", Common.MenuTypes.Games);
+            MainMenuStackPanel_Top.Children.Add(HomeMenu);
+            MainMenuStackPanel_Top.Children.Add(AppMenu);
+            MainMenuStackPanel_Top.Children.Add(GameMenu);
+            LibraryMenu = new MenuUC(Environment.CurrentDirectory + @"\..\..\Images\Menu\library.png", Common.MenuTypes.Libray);
+            HelpMenu = new MenuUC(Environment.CurrentDirectory + @"\..\..\Images\Menu\Help.png", Common.MenuTypes.Games);
+            MainMenuStackPanel_Bottom.Children.Add(LibraryMenu);
+            MainMenuStackPanel_Bottom.Children.Add(HelpMenu);
+
+            HomePage = new HomePage();
+            MainPageFram.Content = HomePage;
         }
     }
 }
